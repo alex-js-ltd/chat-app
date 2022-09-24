e71866f232bc43e048df1c6602529065e8f1d931
Alex Gilbert <alex.js.ltd@gmail.com>
2022-09-24 15:40:55 +0200

    update

---
+++ authenticated-app.tsx
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native';
+import React, {FC} from 'react';
+
+const AuthenticatedApp: FC = () => {
+  return (
+    <View>
+      <Text>AuthenticatedApp</Text>
+    </View>
+  );
+};
+
+export default AuthenticatedApp;
---
+++ unauthenticated-app.tsx
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native';
+import React, {FC} from 'react';
+
+const UnAuthenticatedApp: FC = () => {
+  return (
+    <View>
+      <Text>UnAuthenticatedApp</Text>
+    </View>
+  );
+};
+
+export default UnAuthenticatedApp;
