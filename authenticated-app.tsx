75f300fad4e47c9e254e4b765102331e14321ec6
Alex Gilbert <alex.js.ltd@gmail.com>
2022-09-25 12:01:26 +0200

    update

--- authenticated-app.tsx
+++ authenticated-app.tsx
@@ -1,12 +1,16 @@
-import {View, Text} from 'react-native';
-import React, {FC} from 'react';
+import { View, Text, Button } from 'react-native'
+import React, { FC } from 'react'
+import { useAuth } from './context/auth-context'
 
 const AuthenticatedApp: FC = () => {
-  return (
-    <View>
-      <Text>AuthenticatedApp</Text>
-    </View>
-  );
-};
+    const { logout } = useAuth()
 
-export default AuthenticatedApp;
+    return (
+        <View>
+            <Text>AuthenticatedApp</Text>
+            <Button title="log out" onPress={() => logout()} />
+        </View>
+    )
+}
+
+export default AuthenticatedApp
